dcb14bff7b27c8afd783ab2c472408f4e55ee4b3
Daniel <gilgomezdaniel71@gmail.com>
2025-09-21 23:14:26 +0200

    Automatic push at 2025-09-21 M

--- Cliente/practica3/src/app/components/articulo/articulo.ts
+++ Cliente/practica3/src/app/components/articulo/articulo.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input} from '@angular/core';
+import data from '../../items/articulos.json' assert { type: 'json' };
 
 @Component({
   selector: 'app-articulo',
@@ -7,8 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './articulo.scss'
 })
 export class Articulo {
+  items = data;
 
+  number = input(0);
 
-
-
+  id = this.items.at(this.number())?.id;
+  nombre = this.items.at(this.number())?.nombre;
+  descripcion = this.items.at(this.number())?.descripcion;
+  precio = this.items.at(this.number())?.precio;
+  unidades = this.items.at(this.number())?.unidades;
 }
