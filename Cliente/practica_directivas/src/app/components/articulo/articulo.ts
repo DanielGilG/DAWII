f19c344af39ecc049817195740e5949eec9f424f
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2025-10-11 22:21:32 +0200

    micro

--- Cliente/practica_directivas/src/app/components/articulo/articulo.ts
+++ Cliente/practica_directivas/src/app/components/articulo/articulo.ts
@@ -8,43 +8,42 @@ import data from '../../items/articulos.json' assert { type: 'json' };
   styleUrl: './articulo.scss'
 })
 export class Articulo {
-@Input() productoId: any;
+  @Input() productoId: any;
 
-items = data;
-id : any;
-nombre : any;
-categoria : any;
-descripcion : any;
-unidades : any;
-precio : any;
+  items = data;
+  id: any;
+  nombre: any;
+  categoria: any;
+  descripcion: any;
+  unidades: any;
+  precio: any;
 
-ngOnInit() {
-  console.log('Producto ID recibido:', this.productoId);
-  console.log('Items:', this.items);
+  ngOnInit() {
+    console.log('Producto ID recibido:', this.productoId);
+    console.log('Items:', this.items);
 
-  // Find the item with the matching id
-  const producto = this.items.find(item => item.id === this.productoId);
+    // Find the item with the matching id
+    const producto = this.items.find(item => item.id === this.productoId);
 
-  if (producto) {
-    console.log('Producto encontrado:', producto);
+    if (producto) {
+      console.log('Producto encontrado:', producto);
 
-    this.id = producto.id;
-    this.nombre = producto.nombre;
-    this.categoria = producto.categoria;
-    this.descripcion = producto.descripcion;
-    this.unidades = producto.unidades;
-    this.precio = producto.precio;
+      this.id = producto.id;
+      this.nombre = producto.nombre;
+      this.categoria = producto.categoria;
+      this.descripcion = producto.descripcion;
+      this.unidades = producto.unidades;
+      this.precio = producto.precio;
 
-    console.log('Variables asignadas:');
-    console.log('ID:', this.id);
-    console.log('Nombre:', this.nombre);
-    console.log('Categoría:', this.categoria);
-    console.log('Descripción:', this.descripcion);
-    console.log('Unidades:', this.unidades);
-    console.log('Precio:', this.precio);
-  } else {
-    console.warn('❌ No se encontró ningún producto con el id:', this.productoId);
+      console.log('Variables asignadas:');
+      console.log('ID:', this.id);
+      console.log('Nombre:', this.nombre);
+      console.log('Categoría:', this.categoria);
+      console.log('Descripción:', this.descripcion);
+      console.log('Unidades:', this.unidades);
+      console.log('Precio:', this.precio);
+    } else {
+      console.warn('❌ No se encontró ningún producto con el id:', this.productoId);
+    }
   }
-}
-
 }
