92f07f160e39b45e677887754c8cceda0a653da7
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2025-10-11 22:38:44 +0200

    me voy

---
+++ Cliente/practica_directivas/src/app/components/fila/fila.ts
@@ -0,0 +1,50 @@
+import { Component, Input } from '@angular/core';
+import data from '../../items/articulos.json' assert { type: 'json' };
+
+@Component({
+  selector: 'app-fila',
+  imports: [],
+  templateUrl: './fila.html',
+  styleUrl: './fila.scss'
+})
+export class Fila {
+
+  @Input() productoId: any;
+
+  items = data;
+  id: any;
+  nombre: any;
+  categoria: any;
+  descripcion: any;
+  unidades: any;
+  precio: any;
+
+  ngOnInit() {
+    console.log('Producto ID recibido:', this.productoId);
+    console.log('Items:', this.items);
+
+    // Find the item with the matching id
+    const producto = this.items.find(item => item.id === this.productoId);
+
+    if (producto) {
+      console.log('Producto encontrado:', producto);
+
+      this.id = producto.id;
+      this.nombre = producto.nombre;
+      this.categoria = producto.categoria;
+      this.descripcion = producto.descripcion;
+      this.unidades = producto.unidades;
+      this.precio = producto.precio;
+
+      console.log('Variables asignadas:');
+      console.log('ID:', this.id);
+      console.log('Nombre:', this.nombre);
+      console.log('Categoría:', this.categoria);
+      console.log('Descripción:', this.descripcion);
+      console.log('Unidades:', this.unidades);
+      console.log('Precio:', this.precio);
+    } else {
+      console.warn('❌ No se encontró ningún producto con el id:', this.productoId);
+    }
+  }
+}
