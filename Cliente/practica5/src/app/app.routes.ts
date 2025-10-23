59a0ed1b25b73bed7f8a662f3d71aad9fa7892ab
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2025-10-23 13:20:34 +0200

    micro

--- Cliente/practica5/src/app/app.routes.ts
+++ Cliente/practica5/src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './components/inicio/inicio.component';
 import { Articulos } from './components/articulos/articulos.component';
-import { ErrorComponent } from './components/error/error.component';
 
 export const routes: Routes = [
     {path: '', component: Inicio },
     {path: 'articulos', component: Articulos },
-    {path: '**', component: ErrorComponent },
+    {path: '**', component: Error },
 ];
