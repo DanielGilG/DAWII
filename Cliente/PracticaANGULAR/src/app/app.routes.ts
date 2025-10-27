b310298a40c32ac9b2c48123ebd6728e331f6cce
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2025-10-27 13:26:58 +0100

    Automatic Push from Omarchy

--- Cliente/PracticaANGULAR/src/app/app.routes.ts
+++ Cliente/PracticaANGULAR/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Inicio } from './models/interfaces/inicio/inicio';
 import { PaginaError } from './models/interfaces/pagina-error/pagina-error';
 import { ListaArticulos } from './models/interfaces/lista-articulos/lista-articulos';
+import { VerArticulo } from './models/interfaces/ver-articulo/ver-articulo';
 
 export const routes: Routes = [
     { path: '', component: Inicio },
     { path: 'articulos', component: ListaArticulos},
+    { path: 'articulo', component: VerArticulo},
     { path: '**', component: PaginaError }
 ];
