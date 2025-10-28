0834e25f3552723a42546952fc6d76997da3993a
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2025-10-28 12:50:08 +0100

    CARD SHOWS

--- Cliente/PracticaANGULAR/src/app/app.routes.ts
+++ Cliente/PracticaANGULAR/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { Inicio } from './models/interfaces/inicio/inicio';
 import { PaginaError } from './models/interfaces/pagina-error/pagina-error';
 import { ListaArticulos } from './models/interfaces/lista-articulos/lista-articulos';
 import { VerArticulo } from './models/interfaces/ver-articulo/ver-articulo';
+import { ModificarArticulo } from './models/interfaces/modificar-articulo/modificar-articulo';
+import { BorrarArticulo } from './models/interfaces/borrar-articulo/borrar-articulo';
 
 export const routes: Routes = [
     { path: '', component: Inicio },
     { path: 'articulos', component: ListaArticulos},
-    { path: 'articulo', component: VerArticulo},
+    { path: 'articulo/:id', component: VerArticulo},
+    { path: 'modificar', component: ModificarArticulo},
+    { path: 'borrar', component: BorrarArticulo},
     { path: '**', component: PaginaError }
 ];
--- Cliente/PracticaANGULAR/src/app/models/interfaces/ver-articulo/ver-articulo.ts
+++ Cliente/PracticaANGULAR/src/app/models/interfaces/ver-articulo/ver-articulo.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Articulo, ARTICULOS } from '../../articulo';
+import { ServicioArticulo } from '../../../services/servicio-articulo';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-ver-articulo',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './ver-articulo.html',
   styleUrl: './ver-articulo.css',
 })
 export class VerArticulo {
+  @Input() id! : string;
 
+  articulos: Articulo[] = ARTICULOS;
+
+  constructor(private servicioArticulo: ServicioArticulo) {}
+
+  ngOnInit() {
+    this.servicioArticulo.getArticulo(this.id)
+  }
 }
