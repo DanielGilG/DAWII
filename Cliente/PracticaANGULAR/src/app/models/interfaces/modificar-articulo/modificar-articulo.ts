916e97ceb6072bfab646b57361d2825d309f62ec
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2025-11-02 14:09:50 +0100

    component modificarArticulo

--- Cliente/PracticaANGULAR/src/app/models/interfaces/modificar-articulo/modificar-articulo.ts
+++ Cliente/PracticaANGULAR/src/app/models/interfaces/modificar-articulo/modificar-articulo.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Articulo } from '../../articulo';
 import { RouterLink } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
@@ -8,22 +9,36 @@ import { ServicioArticulo } from '../../../services/servicio-articulo';
 @Component({
   selector: 'app-modificar-articulo',
   imports: [
-    RouterLink
+    RouterLink,
+    ReactiveFormsModule
   ],
   templateUrl: './modificar-articulo.html',
   styleUrl: './modificar-articulo.css',
 })
 export class ModificarArticulo {
   articulo!: Articulo
+  form!: FormGroup;
 
   constructor(
     private miRutaAtiva: ActivatedRoute,
-    private miservicioArticulo: ServicioArticulo
-  ) {}
+    private miservicioArticulo: ServicioArticulo,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
     this.miRutaAtiva
       .params
       .subscribe(params => this.articulo = this.miservicioArticulo.getArticulo(params['id'])!);
+
+    this.form = this.fb.group({
+      id: [this.articulo.id],
+      nombre: [this.articulo.nombre],
+      descripcion: [this.articulo.descripcion],
+      precio: [this.articulo.precio]
+    });
+  }
+
+  onSubmit() {
+    console.log("Form Submitted!");
   }
 }
